8510d3f0067b4ff092622478c1f97d48e24526b8
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2021-10-13 17:29:09 +0200

    [FEAT] Implement equation generation for abs

--- src/main/scala/Equation.scala
+++ src/main/scala/Equation.scala
@@ -1,9 +1,16 @@
 
 case class Equation(ltype: Type, rtype: Type)
 
-def generate_equation(term: Term, stype: Type, env: Map[Var, Type]): List[Equation] =
+def equation_to_string(eq: Equation): String = eq match
+  case Equation(ltype, rtype) => s"${type_to_string(ltype)} = ${type_to_string(rtype)}"
+
+def generate_equation(term: Term, t0: Type, env: Map[Var, Type]): List[Equation] =
   term match
-    case x: Var => List(Equation(env(x), stype))
-    case _ =>
-      println("No more match for generate equation")
-      List()
+    case x: Var =>
+      List(Equation(t0, env(x)))
+
+    case Abs(arg, body) =>
+      val t1 = TVar(Var.fresh_var())
+      val t2 = TVar(Var.fresh_var())
+      val `t1->t2` = Arrow(t1, t2)
+      Equation(t0, `t1->t2`) :: generate_equation(body, t2, env + (arg -> t1))
--- src/test/scala/EquationTest.scala
+++ src/test/scala/EquationTest.scala
@@ -1,13 +1,35 @@
 import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Before
 
 class EquationTest:
-  @Test def should_gen_eq_var(): Unit =
+
+  @Before def initialize(): Unit =
+    Var.last = 1
+
+  @Test def should_get_var_env(): Unit =
     val x = Var("x")
-    val tx = TVar(x)
-    val tv = TVar(Var.fresh_var()) // New env for tx
+    val t0 = TVar(Var("x0"))
 
-    val expected = Equation(tv, tx)
+    generate_equation(x, t0, Map(x -> t0)) match
+      case l: List[Equation] => l foreach {
+        case eq: Equation => assertEquals("x0 = x0", equation_to_string(eq))
+      }
 
-    generate_equation(x, tx, Map(x -> tv)) match
-      case l: List[Equation] => assertEquals(expected, l.head)
+  @Test def should_gen_eq_abs(): Unit =
+    val x = Var("x")
+    val I = Abs(x, x)
+
+    var `0` = Var("x0")
+    var t0 = TVar(`0`)
+
+    var expected = List(Equation(t0, t0))
+
+    generate_equation(I, t0, Map()) match
+      case l: List[Equation] =>
+        l foreach {
+          case Equation(lterm: TVar, rterm: TVar) =>
+            assertEquals(equation_to_string(Equation(lterm, rterm)), "x3 = x2")
+          case Equation(lterm: TVar, rterm: Arrow) =>
+            assertEquals(equation_to_string(Equation(lterm, rterm)), "x0 = (x2 -> x3)")
+        }
