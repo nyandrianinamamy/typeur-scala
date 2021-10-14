e8e7a553fa463e1d8d1c1eb858a6bea3900e1fe1
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2021-10-14 15:08:16 +0200

    [FEAT] Variable occurence check in Arrow

--- src/test/scala/UnificationTest.scala
+++ src/test/scala/UnificationTest.scala
@@ -2,8 +2,22 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class UnificationTest:
-  @Test def occurence_in_var(): Unit =
+  @Test def occurence_in_tvar(): Unit =
     val x = Var("x")
     val t = TVar(x)
 
     assertTrue(occur_check(x, t))
+
+  @Test def occurence_in_arrow(): Unit =
+    val x = Var("x")
+    val y = Var("y")
+    val tx = TVar(x)
+    val ty = TVar(y)
+    val `tx->ty` = Arrow(tx, ty)
+
+    assertTrue(occur_check(x, tx))
+    assertTrue(occur_check(y, ty))
+    assertFalse(occur_check(x, ty))
+    assertFalse(occur_check(y, tx))
+    assertTrue(occur_check(x, `tx->ty`))
+    assertTrue(occur_check(y, `tx->ty`))
