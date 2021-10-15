bb44134c7643fbbd6989ae59b9ff6ed970f6077e
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2021-10-15 11:35:26 +0200

    [FIX] Add env explicitly in the beginning, Map is not mutable anymore

--- src/main/scala/Equation.scala
+++ src/main/scala/Equation.scala
@@ -1,34 +1,26 @@
 import java.util.NoSuchElementException
-import scala.collection.mutable.Map
 
 case class Eq(ltype: Type, rtype: Type):
   override def toString(): String =
     s"${ltype.toString()} = ${rtype.toString()}"
 
-def generate_equation(term: Term): List[Eq] =
-  def generate_equation(term: Term, t0: Type, env: Map[Var, Type]): List[Eq] =
-    term match
-      case x: Var =>
-        env get x match {
-          case Some(a) => Eq(t0, a) :: List()
-          case None =>
-            throw NoSuchElementException(s"Var $x not found in environment")
-        }
+def generate_equation(term: Term, t0: Type, env: Map[Var, Type]): List[Eq] =
+  term match
+    case x: Var =>
+      env get x match {
+        case Some(a) => Eq(t0, a) :: List()
+        case None =>
+          throw NoSuchElementException(s"Var $x not found in environment")
+      }
 
-      case Abs(arg, body) =>
-        val t1 = TVar(Var.fresh_var())
-        val t2 = TVar(Var.fresh_var())
-        val `t1->t2` = Arrow(t1, t2)
-        env(arg) = t1
-        Eq(t0, `t1->t2`) :: generate_equation(body, t2, env)
+    case Abs(arg, body) =>
+      val t1 = TVar(Var.fresh_var())
+      val t2 = TVar(Var.fresh_var())
+      val `t1->t2` = Arrow(t1, t2)
+      Eq(t0, `t1->t2`) :: generate_equation(body, t2, env + (arg -> t1))
 
-      case App(term1, term2) =>
-        val t1 = TVar(Var.fresh_var())
-        val `t1->t0` = Arrow(t1, t0)
+    case App(term1, term2) =>
+      val t1 = TVar(Var.fresh_var())
+      val `t1->t0` = Arrow(t1, t0)
 
-        generate_equation(term1, `t1->t0`, env) ::: generate_equation(term2, t1, env)
-
-  var env: Map[Var, Type] = Map() // Mutable Map
-  val t0 = TVar(Var("x0")) // Type goal alias T0
-
-  generate_equation(term, t0, env)
+      generate_equation(term1, `t1->t0`, env) ::: generate_equation(term2, t1, env)
--- src/test/scala/EquationTest.scala
+++ src/test/scala/EquationTest.scala
@@ -7,6 +7,15 @@ class EqTest:
   @Before def initialize(): Unit =
     Var.last = 1
 
+  @Test def `should generate equation on TVar`(): Unit =
+    val x = Var("x")
+    val but = Var("x0")
+    val Tbut = TVar(but)
+    val env: Map[Var, Type] = Map(x -> Tbut)
+
+    generate_equation(x, Tbut, env) match
+      case h :: q => assertEquals("x0 = x0", h.toString())
+
   /**
    * lamba x.x
    */
@@ -14,7 +23,11 @@ class EqTest:
     val x = Var("x")
     val I = Abs(x, x)
 
-    generate_equation(I) match
+    val `0` = Var("x0")
+    val t0 = TVar(`0`)
+
+    val env: Map[Var, Type] = Map(x -> t0)
+    generate_equation(I, t0, env) match
       case l: List[Eq] =>
         l foreach {
           case Eq(lterm: TVar, rterm: TVar) =>
@@ -34,11 +47,11 @@ class EqTest:
     val `0` = Var("x0")
     val t0 = TVar(`0`)
 
-    val expected = "(x2 -> x0) = (x3 -> x4)" :: "x4 = x3" :: "x2 = x3" :: List()
+    val expected = "(x2 -> x0) = (x3 -> x4)" :: "x4 = x3" :: "x2 = x0" :: List()
 
-    generate_equation(`I x`) match {
+    val env: Map[Var, Type] = Map(x -> t0)
+    generate_equation(`I x`, t0, env) match
       case l: List[Eq] =>
         l foreach {
           case eq: Eq => assertTrue(expected exists (str => str == eq.toString()))
         }
-    }
