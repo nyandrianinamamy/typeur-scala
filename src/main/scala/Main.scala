1b4cc7920c364eb55c1db76cc5112ff8bc081d28
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2021-10-17 19:05:45 +0200

    [FEAT] Implement basic typeur

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,8 @@
+import Typeur._
+
 @main def hello: Unit =
   val x = Var("x")
-  val y = Var("y")
   val I = Abs(x, x)
-  val K = Abs(x, Abs(y, x))
 
-  val KII = App(App(K, I), I)
+  println(typeur(I))
 
-  print_term(KII)
-
-  alpha_conversion(KII) match
-    case app: App => print_term(app)
-
-  val t0 = TVar(Var("x0"))
-
-  val env: Map[Var, Type] = Map(x -> t0)
-
-  generate_equation(KII, t0, env) match
-    case l: List[Eq] => l foreach {
-      case eq: Eq => println(eq.toString())
-    }
---
+++ src/main/scala/Typeur.scala
@@ -0,0 +1,8 @@
+object Typeur {
+  def typeur(term: Term): String =
+    val env: Map[Var, Type] = Map()
+    val eqs = generate_equation(term, TVar.t0, env)
+    unification_etape(eqs) match
+      case List(Eq(ltype, rtype)) => s"Typable avec le type $rtype"
+      case Nil => "Non typable"
+}
