0baada55864bc88140f71db53d014af795526101
Mamy Razafintsialonina <nyandrianinamamy@gmail.com>
2021-11-10 19:09:52 +0100

    [FEAT] add default env to infer()

--- src/main/scala/Typeur.scala
+++ src/main/scala/Typeur.scala
@@ -1,10 +1,10 @@
 object Typeur {
-  def infer(term: Term): String =
-    val env: Map[Var, Type] = Map()
+  def infer(term: Term, env: Map[Var, Type] = Map()): String =
     val eqs = generate_equation(term, TVar.t0, env)
     try {
       unification_etape(eqs) match
         case List(Eq(ltype, rtype)) => s"Typable avec le type $rtype"
+        case _ => throw Error()
     } catch
       case _ => "Non typable"
 }
